8b3a6f91dda6a87d0e7cb47e7287a177cd91aa1c
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-06 09:56:45 +0000

    mandatory task sloved

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            result = ord(str[i]) - 32
+        else:
+            result = ord(str[i])
+        print(f'{result:c}', end="")
+    print()
