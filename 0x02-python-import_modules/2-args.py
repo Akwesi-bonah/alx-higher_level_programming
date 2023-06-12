7d101bc0a02056d17a1816d50ee191722e25fa1e
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-12 16:24:01 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,11 @@ if __name__ == "__main__":
 
     if t_arg == 0:
         print("{} arguments.".format(t_arg))
-    if t_arg >= 1:
+    if t_arg == 1:
+        print("{} argument:".format(t_arg))
+        print("{0}: {1}".format(1, sys.argv[1]))
+    if t_arg > 1:
         print("{} arguments:".format(t_arg))
-    if t_arg > 0:
-        for i in range(1, t_arg + 1):
-            print("{0}: {1}".format(i, sys.argv[i]))
+        if t_arg > 0:
+            for i in range(1, t_arg + 1):
+                print("{0}: {1}".format(i, sys.argv[i]))
