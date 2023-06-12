58797f6bc6f05aa0a428968286f296aad96ac12e
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-12 21:01:56 +0000

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,16 +11,23 @@ int is_palindrome(listint_t **head)
 	listint_t *ptr = NULL;
 	int i = 0, j = 0;
 	int size = 0, flag = 1;
+	int *a = NULL;
 
-
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (flag);
 	}
 
+	ptr = *head;
+	while (ptr->next != NULL)
+	{
+		size++;
+		ptr = ptr->next;
+	}
+
 	a = malloc(sizeof(int) * size + 1);
 	ptr = *head;
-
+	
 	while (ptr != NULL)
 	{
 		a[i] = ptr->n;
@@ -28,13 +35,14 @@ int is_palindrome(listint_t **head)
 		ptr = ptr->next;
 	}
 	j = size;
-
-	for (i = 0; i <= size / 2; i++; j--)
+	for (i = 0; i <= size / 2; i++, j--)
 	{
 		if (a[i] != a[j])
+		{
 			flag = 0;
-
+		}
 	}
+
 	free(a);
 	return (flag);
 }
