48913f1d8483ee10d8b803512cbd6995a0dc6a48
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-14 21:24:33 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if not str(roman_string) or roman_string is None:
+        return 0
+
     roman_letters = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
