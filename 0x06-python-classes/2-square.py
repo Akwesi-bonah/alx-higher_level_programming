495c676a13327c45dde41bdb6ec12f42af645745
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-28 00:20:24 +0000

    Update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,11 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0):
 
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
