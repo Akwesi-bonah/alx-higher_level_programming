f71d71f54157d2f9d2702686a275093753c35af0
Akwesi-bonah <arhinbonnah@gmail.com>
2023-06-29 02:35:03 +0000

    error fixed

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -8,7 +8,7 @@ class Node:
     """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
+        """ Initialize a new Node
 
         Args:
             data (int): The data of the new Node.
@@ -19,7 +19,7 @@ class Node:
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
+        """ Get/set the data of the Node """
         return self.__data
 
     @data.setter
@@ -31,7 +31,7 @@ class Node:
 
     @property
     def next_node(self):
-        """Get/set the next_node of the Node."""
+        """ Get/set the next_node of the Node """
         return self.__next_node
 
     @next_node.setter
@@ -43,14 +43,14 @@ class Node:
 
 
 class SinglyLinkedList:
-    """Represent a singly-linked list."""
+    """ Represent a singly-linked list """
 
     def __init__(self):
-        """Initalize a new SinglyLinkedList."""
+        """ Initalize a new SinglyLinkedList  """
         self.__head = None
 
     def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
+        """Define the print representation of a SinglyLinkedList."""
         if self.__head is None:
             return ""
         nodes = []
@@ -61,14 +61,15 @@ class SinglyLinkedList:
         return "\n".join(nodes)
 
     def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
+        """Insert a new Node to the SinglyLinkedList
+
         The node is inserted into the list at the correct
-        ordered numerical position.
+        ordered numerical position
+
         Args:
             value (Node): The new Node to insert.
         """
         new_node = Node(value)
-
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
