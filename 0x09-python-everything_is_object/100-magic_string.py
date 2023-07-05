94eaac271189b13a5a7fba940cc00896a8a985f5
Akwesi-bonah <arhinbonnah@gmail.com>
2023-07-05 22:58:19 +0000

    fixed task 29 error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pythhon3
+#!/usr/bin/python3
 def magic_string(number=0):
     magic_string.number = getattr(magic_string, "number", 0) + 1
     return ("Holberton, " * magic_string.number)[:-2]
