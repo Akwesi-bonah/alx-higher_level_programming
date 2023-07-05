8d6485393fec22b499d834c41fea6cb0102a8d0c
Akwesi-bonah <arhinbonnah@gmail.com>
2023-07-05 22:13:24 +0000

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/pythhon3
-def magic_string():
-    n = magic_string.__dict__.get('n', 0) + 1
-    magic_string.__dict__['n'] = n
-    return "BestSchool, " * (n - 1) + "BestSchool"
-
-
-if __name__ == "__main__":
-    for i in range(10):
-        print(magic_string())
+def magic_string(number=0):
+    magic_string.number = getattr(magic_string, "number", 0) + 1
+    return ("Holberton, " * magic_string.number)[:-2]
