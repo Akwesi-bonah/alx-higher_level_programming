489e89fe7bd79c1d97d3b4bed97b54bbc71f7090
Akwesi-bonah <arhinbonnah@gmail.com>
2023-09-12 15:05:49 +0000

    task 1 to task 7 complete

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+//#!/usr/bin/node
+const oldclass = require("./5-square");
+class Square extends oldclass {
+  charPrint(C) {
+    if (C === undefined) {
+      C = "X";
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = "";
+      for (let j = 0; j < this.width; j++) {
+        row += C;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return count;
+};
