b874ba5502570060ab1a98e22f28e1861e5c19d2
Akwesi-bonah <arhinbonnah@gmail.com>
2023-10-26 23:28:22 +0000

    upadate

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ script that fetches https://alx-intranet.hbtn.io/status
 if __name__ == "__main__":
     import requests as re
 
-    r = re.get('https://alx-intranet.hbtn.io/status')
-    print("Body response")
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
